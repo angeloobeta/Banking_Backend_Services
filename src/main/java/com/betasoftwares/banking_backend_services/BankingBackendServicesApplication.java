6938fed5d27a1e37bfe4224c92749ddaf5cf4e92
ifeanyichukwuobeta <angeloobeta@hotmail.com>
2023-09-15 11:53:08 +0100

    Done with initial setup

---
+++ src/main/java/com/betasoftwares/banking_backend_services/BankingBackendServicesApplication.java
@@ -0,0 +1,13 @@
+package com.betasoftwares.banking_backend_services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingBackendServicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankingBackendServicesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/betasoftwares/banking_backend_services/BankingBackendServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.betasoftwares.banking_backend_services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingBackendServicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
